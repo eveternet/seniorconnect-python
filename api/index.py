e46355e1c5c8b76d20523791fffbf7a7a4d8b361
pigsye <eayancheong@gmail.com>
2025-05-20 14:50:12 +0800

    Added a backend for once

--- api/index.py
+++ api/index.py
@@ -1,41 +1,26 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
+# Importing blueprints
+from user.interest_groups import interest_groups
+
+# Initialise App
 app = Flask(__name__)
+allowed_origins = os.getenv("ALLOWED_ORIGINS", "").split(",")
+CORS(app, resources={r"/api/*": {"origins": "localhost:5173"}})
+
+# Registering blueprints
+app.register_blueprint(interest_groups, url_prefix='/api/interest_groups')
+
 
 @app.route('/')
 def home():
     return jsonify({"yes": "dis a response"})
 
-@app.route('/api/interest_groups/info')
-def interest_groups_info():
-    return jsonify([
-    {
-      name: "Chess",
-      description:
-        "Play a fun game that keeps your brain sharp. Learn new moves!",
-    },
-    {
-      name: "Checkers",
-      description:
-        "A classic game! Easy to learn, fun to play. Use your thinking.",
-    },
-    {
-      name: "Mahjong",
-      description:
-        "Play with tiles! A classic game from far away. Good for your mind and friends.",
-    },
-    {
-      name: "Poker",
-      description:
-        "Play cards together! A fun game. Use your quick thinking. Just for fun!",
-    },
-    {
-      name: "Yoga",
-      description: "Gentle moves to feel good. Helps you bend and feel calm.",
-    },
-    {
-      name: "Photography",
-      description:
-        "Take pictures! Use your camera or phone. Show what you see.",
-    },
-  ])
+"""
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3000, debug=True)
+"""
