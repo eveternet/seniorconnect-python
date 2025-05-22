50eef1954ee6106dc4103f5dfa492468c1115d46
pigsye <eayancheong@gmail.com>
2025-05-22 17:32:14 +0800

    last try

--- api/user/__init__.py
+++ api/user/__init__.py
@@ -1,8 +1,7 @@
 from flask import Blueprint
 
-user = Blueprint("user", __name__)
+user_blueprints = Blueprint("user", __name__)
 
 from api.user.interest_groups import interest_groups
 
 user.register_blueprint(interest_groups, url_prefix="/interest_groups")
-print("a")
