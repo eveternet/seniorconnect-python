8498b13cea34f871cd9878c23abd8ba2027416e6
pigsye <eayancheong@gmail.com>
2025-05-23 17:15:18 +0800

    fixed __init__.py

--- api/user/__init__.py
+++ api/user/__init__.py
@@ -4,4 +4,4 @@ user_blueprints = Blueprint("user", __name__)
 
 from api.user.interest_groups import interest_groups
 
-user.register_blueprint(interest_groups, url_prefix="/interest_groups")
+user_blueprints.register_blueprint(interest_groups, url_prefix="/interest_groups")
