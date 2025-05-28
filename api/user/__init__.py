a6695a1fcac8d806c3dd39f6ab53d5c6c75e4ab9
pigsye <eayancheong@gmail.com>
2025-05-28 15:42:27 +0800

    AAAAAAAAAAAAAA

--- api/user/__init__.py
+++ api/user/__init__.py
@@ -3,7 +3,7 @@ from flask import Blueprint
 user_blueprints = Blueprint("user", __name__)
 
 from api.user.interest_groups import interest_groups
-from api.user.auth import authUser
+from api.user.auth import user_auth
 
 user_blueprints.register_blueprint(interest_groups, url_prefix="/interest_groups")
-user_blueprints.register_blueprint(authUser, url_prefix="/auth")
+user_blueprints.register_blueprint(user_auth, url_prefix="/auth")
